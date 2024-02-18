c631416d0c34e93014e6a86c575ac85f12398d66
karthicksri111 <155422099+karthicksri111@users.noreply.github.com>
2024-02-18 14:02:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "karthi-s3-tfstate"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "karthi-dynamodb-lock"
   }
 }
